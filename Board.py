5b7c4df8b417f3855fcb166037db784f57258aac
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-04-18 13:21:42 +0330

    class Knight:

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
+import pygame
 class Board:
     def __init__(self, n):
         print("{}*{} board is created!".format(n, n))
@@ -28,6 +29,12 @@ class Board:
 
 class Knight:
     def __init__(self):
+        # Load the image (do this outside the draw_knight function)
+        self.knight_image = pygame.image.load('knight.png').convert()
+        self.knight_image = pygame.transform.smoothscale(self.knight_image, (62, 62))
+
+        print("Knight constructore is called!")
+
 
 
 class Moves:
@@ -36,6 +43,12 @@ class Moves:
 
 
 
+# Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode((10, 10))
+pygame.display.set_caption("Knight's Moves")
+
 board1 = Board(8)
+knight = Knight()
 data = board1.get_data()
 print(data)
